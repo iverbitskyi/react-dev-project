1a1b2da91f3eff88686451ab6dad6c2bb6b6f5d9
TarasMykytiuk228 <158569364+TarasMykytiuk228@users.noreply.github.com>
2024-08-05 18:24:07 +0300

    added buttons (#5)
    Co-authored-by: Taras Mykytiuk <tarasmykytiuk@MacBook-Pro-Taras.local>

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -1,9 +1,27 @@
 import styles from './SideMenu.module.scss';
+import { LuAccessibility } from "react-icons/lu";
 
 export const SideMenu = ({ isMenuOpen }) => {
 	return (
 		<div className={styles.wrapper} style={{ width: isMenuOpen ? '300px' : '50px' }}>
-			Side menu
+			<button className={styles.wrapper_button}>
+				<div className={styles.wrapper_button_icon}>
+					<LuAccessibility />
+				</div>
+				<div style={{ display: !isMenuOpen && "none", width: "250px" }}>zalupa</div>
+			</button>
+			<button className={styles.wrapper_button}>
+				<div className={styles.wrapper_button_icon}>
+					<LuAccessibility />
+				</div>
+				<div style={{ display: !isMenuOpen && "none" }}>zalupa_bobra</div>
+			</button>
+			<button className={styles.wrapper_button}>
+				<div className={styles.wrapper_button_icon}>
+					<LuAccessibility />
+				</div>
+				<div style={{ display: !isMenuOpen && "none" }}>zalupa_root</div>
+			</button>
 		</div>
 	);
 };
