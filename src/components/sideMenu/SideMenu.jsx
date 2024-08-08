294de1d374759f7705461e6a03c28171ab4fb096
Taras <Axotnukk@gmail.com>
2024-08-08 18:12:41 +0300

    added links for side menu

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -1,27 +1,35 @@
-import styles from './SideMenu.module.scss';
+import { Link } from 'react-router-dom';
 import { LuAccessibility } from 'react-icons/lu';
+import styles from './SideMenu.module.scss';
+
 
 export const SideMenu = ({ isMenuOpen }) => {
 	return (
 		<div className={styles.wrapper} style={{ width: isMenuOpen ? '300px' : '50px' }}>
+			<Link to="/">
+				<button className={styles.wrapper_button}>
+					<div className={styles.wrapper_button_icon}>
+						<LuAccessibility />
+					</div>
+					<div className={styles.wrapper_button_text}>Home</div>
+				</button>
+			</Link>
+			<Link to="/login">
 			<button className={styles.wrapper_button}>
 				<div className={styles.wrapper_button_icon}>
 					<LuAccessibility />
 				</div>
-				<div className={styles.wrapper_button_text}>Home</div>
+				<div className={styles.wrapper_button_text}>Login</div>
 			</button>
-			<button className={styles.wrapper_button}>
-				<div className={styles.wrapper_button_icon}>
-					<LuAccessibility />
-				</div>
-				<div className={styles.wrapper_button_text}>Page 1</div>
-			</button>
+			</Link>
+			<Link to="/">
 			<button className={styles.wrapper_button}>
 				<div className={styles.wrapper_button_icon}>
 					<LuAccessibility />
 				</div>
 				<div className={styles.wrapper_button_text}>Page 2</div>
 			</button>
+			</Link>
 		</div>
 	);
 };
