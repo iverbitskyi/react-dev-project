4034b8939eee14e18724aafe184b72f42a8ecca5
iverbitskyi <ivan.verbitskyj@gmail.com>
2024-08-08 18:05:27 +0300

    fix: changed navbar avatar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { SideMenu } from '../sideMenu/SideMenu';
 import styles from './Navbar.module.scss';
 import { CiMenuFries } from 'react-icons/ci';
 import { IoIosClose } from 'react-icons/io';
-import { FaUserCircle } from "react-icons/fa";
+import { FaUserCircle } from 'react-icons/fa';
 
 export const Navbar = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -20,7 +20,7 @@ export const Navbar = () => {
 				</button>
 				<div className={styles.wrapper_user}>
 					<span>User</span>
-					<FaUserCircle size={25} /> 
+					<FaUserCircle size={25} />
 				</div>
 			</div>
 
